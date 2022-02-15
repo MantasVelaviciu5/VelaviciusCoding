e2603656d763081dbf91ec436451a82e0bc473da
Tuzas69 <91669770+Tuzas69@users.noreply.github.com>
2022-02-15 15:33:16 +0200

    v0.1 Mantas Vėlavičius
    vėliau

---
+++ ObjektinisProgrm/ObjektinisProgrm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
