f8ad29166049d63ed35e0c2627c3f3e354de0ccb
Mantas Vėlavičius <mantas.velavicius@mif.stud.vu.lt>
2022-02-22 23:02:51 +0200

    Update ObjektinisProgrm.cpp

--- ObjektinisProgrm/ObjektinisProgrm.cpp
+++ ObjektinisProgrm/ObjektinisProgrm.cpp
@@ -9,9 +9,10 @@
 using std::cout; using std::cin; using std::endl; using std::string;
 using std::left; using std::right; using std::setw; using std::vector;
 
+const int kiekpaz = 10;
 struct Studentas {
     string vardas = "", pavarde = "";
-    int paz[100] = { 0 }, egz = 0;
+    int paz[kiekpaz] = { 0 }, egz = 0;
 };
 
 void ivestis(vector<Studentas>& stud, int ciklas);
@@ -19,55 +20,54 @@ void isvestis(vector<Studentas>& stud, int ciklas);
 double galutskaic(vector<Studentas>& stud, int ciklas); // Funkcija, kuri suskaiciuoja mokinio galutini pazymi;
 double medskaic(vector<Studentas>& stud, int ciklas); // Funkcija, kuri randa mediana;
 
-int kiekpaz = 0;
-
 int main()
 {
     char Status = 't';  // Statuso kintamasis, kuris ziuri, kiek bus mokiniu;
     int ciklas = 0;     // Laikinas kintamasis, kuris pasako, kur esame vektoriuje; 
     vector<Studentas> stud;
-    while (Status == 't'){
+    while (Status == 't') {
         ivestis(stud, ciklas);
-        cout << " Itraukti dar viena studenta? [t / n]?: "; cin >> Status;
+        cout << " Itraukti dar viena studenta? [t / n]? "; cin >> Status;
         ciklas++;
-    }   
+    }
     isvestis(stud, ciklas);
     system("pause");
 }
 
-void ivestis(vector<Studentas>&stud, int ciklas) {
-    cout << " Iveskite studento Varda: "; 
+void ivestis(vector<Studentas>& stud, int ciklas) {
+    cout << " Iveskite studento Varda: ";
     stud.push_back(Studentas());
     cin >> stud[ciklas].vardas;
     cout << " Iveskite studento Pavarde: "; cin >> stud[ciklas].pavarde;
 
-    char stat = 't';
-    while (stat == 't') {
-        cout << " Iveskite ND pazymi " << kiekpaz + 1 << " : "; cin >> stud[ciklas].paz[kiekpaz];
-        kiekpaz++;
-        cout << " Ar norite ivesti dar viena pazymi? [t / n]?: "; cin >> stat;
+
+    srand(static_cast<unsigned int> (std::time(NULL)));
+
+    int i = 0;
+    while (i < kiekpaz) {
+        stud[ciklas].paz[i] = rand() % 10 + 1;
+        i++;
     }
-    cout << " Iveskite egzamino pazymi: "; cin >> stud[ciklas].egz;
-    
+    stud[ciklas].egz = rand() % 10 + 1;
 }
 void isvestis(vector<Studentas>& stud, int ciklas) {
     cout << left << setw(20) << "Pavarde" << left << setw(20) << "Vardas" << left << setw(20) << "Galutinis (Vid.)" << left << setw(20) << "Galutinis (Med.)" << endl;
     cout << " --------------------------------------------------------------------------- " << endl;
-    for (int i = 0; i < ciklas; i++) 
+    for (int i = 0; i < ciklas; i++)
         cout << endl << left << setw(20) << stud[i].pavarde << left << setw(20) << stud[i].vardas << left << setw(20) << std::fixed << std::setprecision(2) << galutskaic(stud, i) <<
-            left << setw(20) << std::fixed << std::setprecision(2) << medskaic(stud, i) << endl;
+        left << setw(20) << std::fixed << std::setprecision(2) << medskaic(stud, i) << endl;
 }
 double galutskaic(vector<Studentas>& stud, int ciklas) {
     int suma = 0;
     for (int i = 0; i < kiekpaz; i++)
         suma += stud[ciklas].paz[i];
-     return 0.4 * (suma / kiekpaz) + 0.6 * stud[ciklas].egz;
-     
+    return 0.4 * (suma / kiekpaz) + 0.6 * stud[ciklas].egz;
+
 }
 double medskaic(vector<Studentas>& stud, int ciklas) {
     std::sort(stud[ciklas].paz, stud[ciklas].paz + kiekpaz);
     if (kiekpaz % 2 == 0)
         return (stud[ciklas].paz[(kiekpaz / 2) - 1] + stud[ciklas].paz[kiekpaz / 2]) / 2.0;
     else return stud[ciklas].paz[(kiekpaz - 1) / 2];
-     
+
 }
